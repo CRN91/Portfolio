3cdd2ef9b78f20a5832baa54d45f82e27a5be36c
CRN91 <ak824@exeter.ac.uk>
2024-10-12 14:13:37 +0100

    Generating html elements in the board with JS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import './App.css';
 import React, { useState } from "react";
 
-
-
 function Square({value, onSquareClick}) {
 
   return (
@@ -38,24 +36,32 @@ function Board({xIsNext, squares, onPlay}) {
     onPlay(nextSquares);
   }
 
+  function generateSquare(i) {
+    return <Square value={squares[i]} onSquareClick={() => handleClick(i)} />;
+  }
+
+  function generateBoardRow(row) {
+    return <div className="board-row">
+      {generateSquare(row * 3)}
+      {generateSquare(row * 3 + 1)}
+      {generateSquare(row * 3 + 2)}
+    </div>;
+  }
+
+  function generateBoard() {
+    return <>
+      {generateBoardRow(0)}
+      {generateBoardRow(1)}
+      {generateBoardRow(2)}
+    </>;
+  }
+
+  
+
   return (
     <>
     <div className="status">{status}</div>
-    <div className="board-row">
-     <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
-     <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
-     <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-    </div>
-    <div className="board-row">
-      <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
-      <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
-      <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-    </div>
-    <div className="board-row">
-      <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
-      <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
-      <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
-    </div>
+    {generateBoard()}
   </>
   )
 }
