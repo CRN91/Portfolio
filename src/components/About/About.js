7308eeb7c17a8fb1839e7f7d09e85b559b701886
CRN91 <ak824@exeter.ac.uk>
2024-10-15 11:16:52 +0100

    test github pages push

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -8,7 +8,7 @@ const About = () => {
         <h1 className="name-title">Adam Kaizra</h1>
         <p className="course-text">MSci Computer Science</p>
         <p className="specialization-text">I am a software engineer currently studying robotics, simulation, computer vision and machine learning. Previous topics I have studied include network science, data analysis and optimisation.</p>
-        <div style={{left: '25%', position: 'relative'}}>
+        <div className="cv">
             <DownloadCV />
         </div>
         </>
