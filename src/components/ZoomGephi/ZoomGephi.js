9ce2be72d7538c21935f9023fe30e1ae57c7c7f5
CRN91 <ak824@exeter.ac.uk>
2024-10-29 11:19:40 +0000

    Removed image zoom feature on mobile as it was buggy and mobile users can easily and intuitively zoom with inbuilt web browser features. Might need to have a look at this again as it is a bit laggy on desktop too

--- src/components/ZoomGephi/ZoomGephi.js
+++ src/components/ZoomGephi/ZoomGephi.js
@@ -1,15 +1,41 @@
-import React from "react"
+import React from "react";
 import ImageZoom from "react-image-zooom";
 import graphSVG from './alliance2018.svg';
 import './ZoomGephi.css';
 
 function ZoomGephi() {
+  const [isMobile, setIsMobile] = React.useState(window.innerWidth <= 768);
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <div>
-      <h2 className="graphimg1-title"> Visualisation of the Alliance Network in 2018</h2>
-      <br></br>
-      <h4 className="graphimg3-title"> Tip: Click on the image to zoom and pan </h4>
-      <ImageZoom className="graph-image2" src={graphSVG} alt="Alliance Network in 2018" zoom="250"/>
+    <div className="zoom-gephi-container">
+      <h2 className="graphimg1-title">Visualisation of the Alliance Network in 2018</h2>
+      <br />
+      {!isMobile && (
+        <h4 className="graphimg3-title">Tip: Click on the image to zoom and pan</h4>
+      )}
+      {isMobile ? (
+        <img 
+          src={graphSVG} 
+          alt="Alliance Network in 2018"
+          className="graph-image2 static-image"
+        />
+      ) : (
+        <ImageZoom 
+          className="graph-image2" 
+          src={graphSVG} 
+          alt="Alliance Network in 2018" 
+          zoom="250"
+        />
+      )}
     </div>
   );
 }
